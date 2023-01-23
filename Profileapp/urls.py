07df93d44bc0456a3f12c20097924f597e1355ba
apiwat888 <apiwat.ml@rmuti.ac.th>
2023-01-23 15:18:17 +0700

    second commit 23 ม.ค 66

--- Profileapp/urls.py
+++ Profileapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from Profileapp import views
 
 urlspatterns = [
+    path('', views.home, name='home'),
     path('edu', views.edu, name='edu'),
     path('interests', views.interests, name='interests'),
     path('product', views.product, name='product'),
